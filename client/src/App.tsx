34e02b9122822a1fe806dce803e52ea43a696c1c
bbabi0901 <69451349+bbabi0901@users.noreply.github.com>
2023-04-25 15:34:14 +0900

    client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,49 @@
-import React from 'react';
+// modules
+import React, { useState } from 'react';
+import { Layout, Space } from 'antd';
 
-function App() {
-  return <div className="App">hi</div>;
-}
+// styles
+import { styles } from './assets/styles/styles';
+
+// components
+// import Router from './router/Router';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+// import Sidebar from './components/Sidebar/Sidebar';
+
+const { Content } = Layout;
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Space direction="vertical" style={{ width: '100%' }}>
+        <Layout>
+          <Layout
+            style={{
+              height: '100%',
+              background: `${styles.white} )`,
+              color: `${styles.very_dark_blue_line}`,
+              gap: `${styles.space_8}`,
+            }}
+          >
+            <Header />
+            <Content
+              style={{
+                padding: '0 50px',
+                display: 'flex',
+                flexDirection: 'column',
+                gap: `${styles.space_9}`,
+              }}
+              className="site-layout-content"
+            >
+              Content
+            </Content>
+            <Footer />
+          </Layout>
+        </Layout>
+      </Space>
+    </div>
+  );
+};
 
 export default App;
---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Layout } from 'antd';
+
+const Header: React.FC = () => {
+  return <Layout.Header style={{ textAlign: 'center' }}>Header</Layout.Header>;
+};
+
+export default Header;
